d3f1d9aa4491fd9abb88a34e6493922b25bf942d
nagi1999a <nagi1999a.daily@gmail.com>
2024-04-18 15:41:35 +0800

    Refactor code and update readme

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # wireshark_pseudo_ta
-A lua plugin for Wireshark to show inferred/pseudo transmitter address for ACK, CTS, ..., with a single field for filtering ta&amp;ra.
-
-## About
+A Lua plugin for Wireshark to show inferred/pseudo transmitter address for ACK, CTS, ..., with a single field for filtering ta&amp;ra.
 Inspired by and Modified from [wireshark-tcpextend](https://github.com/gaddman/wireshark-tcpextend)
 
-Some protocols in 802.11 do not have transmitter fields, because sometimes we do not need it. However, it may sometimes cause annoying things while analyzing sniffer logs. For example, we may need to include ACK frames to know whether receiver has received a frame between traffics of STA1 & STA2. But since ACK frame does not have trasmitter address, we cannot simply use a display filter like `(wlan.addr == <STA1>) && (wlan.addr == <STA2>)`. When we include ACKs by extends the filter with `((wlan.ra == <STA1>) || (wlan.ra == <STA2>)) && (wlan.fc.type_subtype == 0x001d)`, all Acks that may not issue by STA1 or STA2 will be included.
+## About
+Some protocols in 802.11 do not have transmitter fields because we do not need them. However, it may sometimes cause annoying things while analyzing sniffer logs. For example, we may need to include ACK frames to know whether the receiver has received a frame between the traffic of STA1 & STA2. But since the ACK frame does not have a transmitter address, we cannot simply use a display filter like `(wlan.addr == <STA1>) && (wlan.addr == <STA2>)`. When we include ACKs by extends the filter with `((wlan.ra == <STA1>) || (wlan.ra == <STA2>)) && (wlan.fc.type_subtype == 0x001d)`, all Acks that may not issue by STA1 or STA2 will be included.
 
-The plugin will first check whether the `wlan.ta` field is exists. If exist, it will directly use the value as `p-ta.ta`. Otherwise, it will try to infer a pseudo transmitter address as `p-ta.ta`, when the transmitter of the previous frame is equal to the receiver of the current frame. It also provide an additional field that contains transmitter and receiver address. Thus, when we want to filter the traffic between STA1 & STA2 with ACKs included, we can write a much shorter filter `(p-ta.addr == <STA1>) && (p-ta.addr == <STA2>)`.
+The plugin will first check whether the `wlan.ta` field exists. If it exists, it will directly use the value as `p-ta.ta`. Otherwise, it will try to infer a pseudo transmitter address as `p-ta.ta`, when the transmitter of the previous frame is equal to the receiver of the current frame. It also provides an additional field that contains the transmitter and receiver address. Thus, when we want to filter the traffic between STA1 & STA2 with ACKs included, we can write a much shorter filter `(p-ta.addr == <STA1>) && (p-ta.addr == <STA2>)`.
 
-If there is any question or bug, please feel free to open issues!
+If there are any questions or bugs, please feel free to open up the issues!
 
 ## Functionality
-1. Provide a pseudo transmitter address based on previous frame.
+1. Provide a pseudo transmitter address based on the previous frame.
 2. Provide a field to distinguish whether the address is pseudo.
-3. Provide an additional field to combine transmitter and receiver address.
+3. Provide an additional field to combine the transmitter and receiver addresses.
 
-## Enable Condition
-1. wlan.fc.type_subtype == 0x001d (ACK)
-2. wlan.fc.type_subtype == 0x001c (CTS)
+## Support types
+1. ACK
+2. RTS-CTS
+3. CTS-to-self
 
 ## How to use
-Place in [Wireshark Plugin Folder](https://www.wireshark.org/docs/wsug_html_chunked/ChPluginFolders.html) and restart Wireshark.
+Place the Lua script in [Wireshark Plugin Folder](https://www.wireshark.org/docs/wsug_html_chunked/ChPluginFolders.html) and restart Wireshark.
 
 ## Fields
 | Field            | Type    | Meaning                                                                |
@@ -29,8 +29,10 @@ Place in [Wireshark Plugin Folder](https://www.wireshark.org/docs/wsug_html_chun
 | p-ta.taStr       | string  | String of Transmitter Address <br> with parentheses to indicate pseudo |
 | p-ta.isPseudo    | bool    | Is Pseudo                                                              |
 | p-ta.isPseudoStr | string  | String to indicate pseudo                                              |
-| p-ta.addr        | address | TA & RA                                                                |
+| p-ta.addr        | address | TA & RA$^{[1]}$     |
+
+[1]: The plugin will try to contain the RA of the next frame for CTS-to-self frames. However, since live capture has a different 2-pass dissector process, `p-ta.addr` will not guarantee the RA of the next frame will be included. The same situation appears when reloading the Lua script using `Analyze > Reload Lua Plugins` when there is a display filter set.
 
 ## Screenshot
-`Network_Join_Nokia_Mobile.pcap` is from [Wireshark Wiki](https://wiki.wireshark.org/SampleCaptures#wifi-wireless-lan-captures-80211)
+Pcap (`Network_Join_Nokia_Mobile.pcap`) is from [Wireshark Wiki](https://wiki.wireshark.org/SampleCaptures#wifi-wireless-lan-captures-80211)
 ![Screenshot](screenshot.png)
--- pseudo_ta.lua
+++ pseudo_ta.lua
@@ -1,41 +1,47 @@
 -- A Wireshark LUA script to display pseudo TA Information
 
+--- Fields that used to extract previous frame data
 local f_wlan_ta = Field.new("wlan.ta")
 local f_wlan_ra = Field.new("wlan.ra")
+local f_wlan_type_subtype = Field.new("wlan.fc.type_subtype")
 
+--- Define enable conditions
+local conditions = {
+    ["ACK"]         = true,
+    ["RTS-CTS"]         = true,
+    ["CTS-to-self"] = true,
+}
+
+--- Define a new protocol
 local p_pseudoTA = Proto("p-ta", "Pseudo TA Information")
 
+--- Fields in the new protocol
 local F_ta = ProtoField.ether("p-ta.ta", "Transmitter Address")
 local F_taStr = ProtoField.string("p-ta.taStr", "String of Transmitter Address with parentheses to indicate pseudo")
 local F_isPseudo = ProtoField.bool("p-ta.isPseudo", "Is Pseudo")
 local F_isPseudoStr = ProtoField.string("p-ta.isPseudoStr", "String to indicate pseudo")
 local F_addr = ProtoField.ether("p-ta.addr", "TA & RA")
 
+--- Registor new protocol fields
 p_pseudoTA.fields = {F_ta, F_taStr, F_isPseudo, F_isPseudoStr, F_addr}
 
-local f_ack = Field.new("wlan.fc.type_subtype")
-local f_rts = Field.new("wlan.fc.type_subtype")
-local valid_fields_f = {
-    f_ack,
-    f_rts,
-}
-local valid_fields_v = {
-    0x001d,
-    0x001c
-}
-
+--- Since dissectors cannot access information of other frames, so maintain global tables to save things
 local org_data = {}
 local data = {}
 
+--- Init function
 local function reset_stats()
 	org_data = {}
 	org_data.ta = {}
 	org_data.ra = {}
+    org_data.type_subtype = {}
+    org_data.n_ta = {}
+    org_data.n_ra = {}
 
     data = {}
     data.ta = {}
     data.isPseudo = {}
-    data.isValid = {}
+    data.condition = {}
     data.addr = {}
 end
 
@@ -43,57 +49,95 @@ function p_pseudoTA.init()
 	reset_stats()
 end
 
-function p_pseudoTA.dissector(buffer,pinfo,tree)
-
+--- Main dissector function
+function p_pseudoTA.dissector(buffer, pinfo, tree)
+    --- Get the information of current frame
     local ta = f_wlan_ta()
     local ra = f_wlan_ra()
+    local type_subtype = f_wlan_type_subtype()
     local num = pinfo.number
 
+    --- Data init should only run once
     if not pinfo.visited then
+        if ta then org_data.ta[num] = ta.value end
+        if ra then org_data.ra[num] = ra.value end
+        if type_subtype then org_data.type_subtype[num] = type_subtype.value end
+        if (num > 0) and ta then org_data.n_ta[num-1] = ta.value end
+        if (num > 0) and ra then org_data.n_ra[num-1] = ra.value end
+
+        --- Init addr
         data.addr[num] = {}
-
-        if ra then
-            org_data.ra[num] = ra.value
-            data.addr[num][#data.addr[num]+1] = ra.value
+        if org_data.ra[num] then
+            data.addr[num][1] = org_data.ra[num]
         end
+
+        --- check whether need to perform TA inferring
+        data.isPseudo[num] = false
         
-        if ta then
-            org_data.ta[num] = ta.value
-            data.addr[num][#data.addr[num]+1] = ta.value
-            data.ta[num] = ta.value
+        --- Special Case: TA exists in current frame
+        if org_data.ta[num] then
             data.isPseudo[num] = false
-        elseif (num > 0) and (org_data.ta[num-1]) and (org_data.ra[num-1]) and (org_data.ra[num]) and (org_data.ra[num] == org_data.ta[num-1]) then
-            data.addr[num][#data.addr[num]+1] = org_data.ra[num-1]
-            data.ta[num] = org_data.ra[num-1]
-            data.isPseudo[num] = true
+            data.ta[num] = org_data.ta[num]
+            data.addr[num][#data.addr[num]+1] = org_data.ta[num]
         end
 
-        data.isValid[num] = false
-        for index, f_valid in ipairs(valid_fields_f) do
-            local field = f_valid()
-            if field and (field.value == valid_fields_v[index]) then
-                data.isValid[num] = true
+        --- 1. ACK
+        if org_data.type_subtype[num] == 0x001d then
+            assert(org_data.ra[num], "ACK frame should have RA value!")
+            if (org_data.ra[num] == org_data.ta[num-1]) and (org_data.ra[num-1]) then
+                data.isPseudo[num] = true
+                data.condition[num] = "ACK"
+                data.ta[num] = org_data.ra[num-1]
+                data.addr[num][#data.addr[num]+1] = org_data.ra[num-1]
+            end
+        end
+
+        --- CTS Related
+        if org_data.type_subtype[num] == 0x001c then
+            assert(org_data.ra[num], "CTS frame should have RA value!")
+            --- 2. RTS-CTS
+            if (org_data.type_subtype[num-1] == 0x001b) then
+                if (org_data.ra[num] == org_data.ta[num-1]) and (org_data.ra[num-1]) then
+                    data.isPseudo[num] = true
+                    data.condition[num] = "RTS-CTS"
+                    data.ta[num] = org_data.ra[num-1]
+                    data.addr[num][#data.addr[num]+1] = org_data.ra[num-1]
+                end
+            --- 3. CTS-to-self
+            else
+                data.isPseudo[num] = true
+                data.condition[num] = "CTS-to-self"
+                data.ta[num] = org_data.ra[num]
             end
         end
     end
 
     if pinfo.visited then
-        if data.ta[num] and (data.isPseudo[num] == true) and (data.isValid[num]) then
-            tree:add(F_ta, data.ta[num]):append_text(' (Pseudo)'):set_generated()
-            tree:add(F_taStr, '(' .. tostring(data.ta[num]) .. ')'):set_hidden():set_generated()
-            tree:add(F_isPseudo, true):set_hidden():set_generated()
-            tree:add(F_isPseudoStr, 'T'):set_hidden():set_generated()
-        elseif data.ta[num] and (data.isPseudo[num] == false) then
-            tree:add(F_ta, data.ta[num]):set_generated()
-            tree:add(F_taStr, tostring(data.ta[num])):set_hidden():set_generated()
-            tree:add(F_isPseudo, false):set_hidden():set_generated()
-            tree:add(F_isPseudoStr, ''):set_hidden():set_generated()
-        end
-        for index, value in ipairs(data.addr[num]) do
-            tree:add(F_addr, value):set_hidden():set_generated()
+        --- Trick for CTS-to-self, add RA in the next frame to the current frame addr
+        --- Note: Only works for static pcap, live capture does not guarantee to perform 2-pass.
+        if (org_data.type_subtype[num] == 0x001c) then
+            assert(org_data.ra[num], "CTS frame should have RA value!")
+            if (org_data.ra[num] == org_data.n_ta[num]) then
+                data.addr[num][#data.addr[num]+1] = org_data.n_ra[num]
+            end
         end
     end
-    
+
+    if data.ta[num] and (data.isPseudo[num] == true) and (conditions[data.condition[num]] == true) then
+        tree:add(F_ta, data.ta[num]):append_text(' (Pseudo)'):set_generated()
+        tree:add(F_taStr, '(' .. tostring(data.ta[num]) .. ')'):set_hidden():set_generated()
+        tree:add(F_isPseudo, true):set_hidden():set_generated()
+        tree:add(F_isPseudoStr, 'T'):set_hidden():set_generated()
+    elseif data.ta[num] and (data.isPseudo[num] == false) then
+        tree:add(F_ta, data.ta[num]):set_generated()
+        tree:add(F_taStr, tostring(data.ta[num])):set_hidden():set_generated()
+        tree:add(F_isPseudo, false):set_hidden():set_generated()
+        tree:add(F_isPseudoStr, ''):set_hidden():set_generated()
+    end
+    for index, value in ipairs(data.addr[num]) do
+        tree:add(F_addr, value):set_hidden():set_generated()
+    end
 end
 
+--- Registor post-dissector
 register_postdissector(p_pseudoTA)
